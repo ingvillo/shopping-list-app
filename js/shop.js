b984787b368a373d6fb3fdd9bbf6542c4d2b5c48
ingvillo <ingvilloddsen@gmail.com>
2015-08-07 10:55:57 +0200

    ticked items append to list, new items prepend to list

--- js/shop.js
+++ js/shop.js
@@ -3,14 +3,14 @@ function addListItem() {
     
      var write = $('#newItem').val();
      var list = $('#itemList');
-     var item = $('<li><button class="checkbox">&#x2713;</button><span class="list">' + write + '</span><button class="delete">X</button></li>');
+     var item = $('<li><button class="checkbox">&#x2713;</button><span class="list">' + write + '</span><button class="delete">x</button></li>');
      var newItem = $("#newItem");
 
      if (write.length === 0  || write.length > 22) {
         return false;
     }
     
-    list.append(item);
+    list.prepend(item);
     $(newItem).val('');
 }
 
@@ -24,8 +24,19 @@ function deleteItem() {
 /* Ticks off list item */
 function tickItem() {
 
-    $(this).parent().toggleClass('tick');
-}
+    $(this).parent().toggleClass('tick'); 
+    /* Moves ticked items to the bottom of list */
+	$('#itemList').find('.tick').appendTo('#itemList');}
+
+
+
+
+
+
+
+
+
+
 
 
 /* Clears all items on list */
@@ -33,6 +44,7 @@ $(document).on('click', '#resetButton', function(){
     $('#itemList').empty();
     submissions = 0;
 });
+
 
 
 /* Adds item to list */
@@ -51,8 +63,10 @@ $(function() {
         }
   
     });
-    
-        
-  
+     
     
 });
+
+
+
+
